3a10753fc64eb4cc865de6220e4c409f5881ed9b
Numbsku11 <mswift823@gmail.com>
2025-03-06 14:06:06 +0000

    added more options, clear, junk, mkdir, and debug. added debug logic for sections where needed and reduce clutter in the terminal when testing

--- src/switchtesting.cpp
+++ src/switchtesting.cpp
@@ -1,21 +1,40 @@
 #include <headers.h> 
+bool debugBool = 0; 
+
+void csvHandle()
+{
+    
+}
+
+void mkdirHandle()
+{
+
+}
+
+
+
+
+
+
 
 // list of all the possible options and enums for a string
 enum class StringCodes
 {
-    eating,
-    drinking,
-    sleeping,
+    mkdirtest,
+    debug,
+    junkPrinter,
     exit,
+    clear,
     unknown
 };
 
 // matches the strings and returns one of enumed options or an error case 
 StringCodes hashString(const std::string& SourceString) 
 {
-    if (SourceString == "") return StringCodes::eating;
-    if (SourceString == "")  return StringCodes::drinking;
-    if (SourceString == "")  return StringCodes::sleeping;
+    if (SourceString == "mkdir") return StringCodes::mkdirtest;
+    if (SourceString == "debug")  return StringCodes::debug;
+    if (SourceString == "clear")  return StringCodes::clear;
+    if (SourceString == "junk")  return StringCodes::junkPrinter;
     if (SourceString == "") return StringCodes::exit;
     return StringCodes::unknown;
 }
@@ -24,27 +43,50 @@ void processCommand(const std::string& command)
 {
     switch (hashString(command)) 
     {
-        case StringCodes::eating:
-            std::cout << "e" << std::endl;
+        case StringCodes::mkdirtest:
+            if (debugBool == 1)
+            {
+                std::cout << "debug: directory creation, path & deletion" << std::endl;
+            }
+            mkdirHandle(); 
         break;
         
-        case StringCodes::drinking:
-            std::cout << "b" << std::endl;
+        case StringCodes::debug:
+            //simple logic to enable or disable the debug flag 
+            if (debugBool != 1)
+            {
+                debugBool = 1;
+                std::cout << "debug set to:\t" << debugBool << std::endl;
+            }
+            
+            else 
+            {
+                debugBool = 0;
+                std::cout << "debug set to:\t" << debugBool << std::endl; 
+            }
         break;
         
-        case StringCodes::sleeping:
-            std::cout << "c" << std::endl;
+        
+        case StringCodes::junkPrinter:
+            // test to ensure that the clear func removes everything in the terminal
+            std::cout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Donec commodo ante nec diam venenatis consequat. Mauris tristique lacus id neque faucibus laoreet.\n Etiam urna tortor, cursus vitae eros eu, tristique vulputate dolor.\n Phasellus at metus ipsum. Praesent in orci vitae odio viverra vehicula a id sapien.\n Maecenas sapien est, sodales a erat at, dictum cursus tortor.\n Etiam sagittis est quis augue efficitur, ultricies consectetur metus fermentum. Integer nisi sapien, elementum vel nibh sit amet, interdum ultricies neque.\n Aliquam lobortis consectetur ex vestibulum accumsan. \nDonec urna purus, venenatis sit amet diam vel, accumsan gravida leo. Ut et aliquet lacus."
+            << std::endl;
         break;
         
         case StringCodes::exit:
             exit(0);
         break; 
         
+        case StringCodes::clear:
+            //terminal code to clear everything (working on linux and some later versions of windows)
+            printf("\033c"); 
+        break;
+
         default:
+            // case for if nothing is recognised from user input 
             std::cout << "Error, please re-enter option" << std::endl;
     }
 }
-
 
 
 int main()
@@ -58,7 +100,10 @@ int main()
         std::transform(starterString.begin(),starterString.end(),
         starterString.begin() ,[](unsigned char c) {return std::tolower(c);});
     
-        std::cout << "debug: lowerstring: \t\n";
+        if (starterString != "debug" && debugBool == 1)
+    {
+        std::cout << "debug: lowerstring: \t" << starterString << std::endl;
+    }
 
         processCommand(starterString);
     }
